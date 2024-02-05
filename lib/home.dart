524035c3f3f8475278ef5cc365e91de4071211e4
Nirsu <meru.fontaine@gmail.com>
2024-02-06 00:47:11 +0100

    [FEAT]: move home page to different files

---
+++ lib/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:quick_querty/core/themes/extensions/theme_extension.dart';
+
+/// Home page widget.
+class HomePage extends StatefulWidget {
+  /// Constructor for the home page widget.
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            if (_counter >= 1)
+              Text(
+                'This is cool !',
+                style: context.theme.textTheme.bodySmall,
+              ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quick_querty/core/themes/default_theme.dart';
-import 'package:quick_querty/core/themes/extensions/theme_extension.dart';
+import 'package:quick_querty/home.dart';
 
 void main() {
   runApp(const QuickQuerty());
@@ -15,74 +15,9 @@ class QuickQuerty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quick Querty',
       theme: defaultTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-/// Basic home page widget.
-class MyHomePage extends StatefulWidget {
-  /// Constructor for the home page widget.
-  const MyHomePage({
-    required this.title,
-    super.key,
-  });
-
-  /// The title of the home page.
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(4),
-          child: Container(
-            color: context.theme.colorScheme.secondary,
-            height: 1,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            if (_counter >= 1)
-              Text(
-                'This is cool !',
-                style: context.theme.textTheme.bodySmall,
-              ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const HomePage(),
     );
   }
 }
